49eae8c2c40a5ae2257b16095d75c631c6b68480
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2022-05-20 00:12:08 -0700

    pushign rewards tuff

--- render.py
+++ render.py
@@ -1,4 +1,4 @@
-import gym
+import random
 import torch
 import cv2
 
@@ -16,8 +16,6 @@ sys.path.append(os.getcwd())
 
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
 from sklearn.neighbors import NearestNeighbors
-
-#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 def add_camera_light():
     bpy.ops.object.light_add(type='SUN', radius=1, location=(0,0,0))
@@ -55,7 +53,6 @@ def set_render_settings(engine, render_size, generate_masks=True):
     scene.render.image_settings.file_format='JPEG'
     scene.view_settings.exposure = 1.3
     scene.render.image_settings.file_format='JPEG'
-    #scene.cycles.samples = 10
     scene.cycles.samples = 1
     scene.view_settings.view_transform = 'Raw'
     scene.cycles.max_bounces = 1
@@ -77,9 +74,7 @@ def initialize_renderer():
     links = tree.links
     render_node = tree.nodes["Render Layers"]
 
-    #rl = tree.nodes.new('CompositorNodeRLayers')   
     vl = tree.nodes.new('CompositorNodeViewer')   
-    #vl.use_alpha = False
 
     id_mask_node = tree.nodes.new(type="CompositorNodeIDMask")
 
@@ -90,19 +85,21 @@ def initialize_renderer():
     links.new(id_mask_node.outputs[0], composite.inputs["Image"])
 
     links.new(id_mask_node.outputs[0], vl.inputs[0])  # link Renger Image to Viewer Image
-    #links.new(id_mask_node.outputs[2], vl.inputs[1])  # link Render Z to Viewer Alpha
 
 def render(episode):
     bpy.context.scene.render.filepath = 'masks/%05d.jpg'%episode
     bpy.ops.render.render(write_still=True)
     image = None
-    #bpy.ops.render.render(write_still=False)
+
     #pixels = np.array(bpy.data.images['Viewer Node'].pixels)
     #width = bpy.context.scene.render.resolution_x
     #height = bpy.context.scene.render.resolution_y
     #image = pixels.reshape(height,width,4)
     #image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
-    #cv2.imwrite('masks/%05d.jpg'%episode, image)
+    ##cv2.imshow('img', image)
+    ##cv2.waitKey(0)
+    ##cv2.imwrite('masks/%05d.jpg'%episode, image)
+
     return image
 
 def make_fork(path_to_fork_stl):
@@ -121,7 +118,6 @@ def make_fork(path_to_fork_stl):
     fork.rigid_body.use_deform = True
     fork.rigid_body.friction = 1
     fork.rigid_body.collision_margin = 0.01
-
     return fork
 
 def make_table(params):
@@ -140,17 +136,12 @@ def make_pusher():
     rotation = (0,np.pi/2,0)
     bpy.ops.mesh.primitive_plane_add(size=1, location=location, rotation=rotation)
     pusher = bpy.context.object
-    #pusher.scale = (1,1,0.1)
-    #bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
     bpy.ops.object.modifier_add(type='COLLISION')
     bpy.ops.rigidbody.object_add()
     pusher.rigid_body.mass = 25
     pusher.rigid_body.kinematic = True
     pusher.rigid_body.type = 'PASSIVE'
 
-    #pusher.collision.damping = 0.1
-    #pusher.collision.thickness_outer = 0.3
-    #pusher.collision.thickness_inner = 0.5
     pusher.collision.damping = 0.5
     pusher.collision.thickness_outer = 0.3
     pusher.collision.thickness_inner = 0.5
@@ -168,7 +159,6 @@ def add_softbody_physics(noodles):
     bpy.context.view_layer.objects.active = noodles
     bpy.ops.object.modifier_add(type='SOFT_BODY')
     noodles.modifiers["Softbody"].settings.mass = 1.3
-    #noodles.modifiers["Softbody"].settings.ball_stiff = 1
     noodles.modifiers["Softbody"].settings.ball_stiff = 1.2
     noodles.modifiers["Softbody"].settings.ball_damp = 0.8
 
@@ -183,9 +173,6 @@ def add_softbody_physics(noodles):
     bpy.context.object.data.bevel_mode = 'OBJECT'
 
     bpy.ops.object.select_all(action='DESELECT')
-
-    #noodles.modifiers["Softbody"].settings.step_max = 250
-
     return noodles
 
 def annotate(points):
@@ -199,15 +186,23 @@ def annotate(points):
         pixels.append(pixel)
     return pixels
 
+def get_reward(noodles):
+    freeze_softbody_physics(noodles)
+    hull_2d, center_2d, furthest_2d, area, densest_3d = noodle_state(noodles)
+    #print('REWARD: area, noodles', area, len(noodles.data.splines))
+    add_softbody_physics(noodles)
+    return area, len(noodles.data.splines)
+
 def make_noodle():
-    location = np.random.uniform(-0.3,0.3,3)
+    #location = np.random.uniform(-0.3,0.3,3)
+    location = np.random.uniform(-0.6,0.6,3)
     location[2] = np.random.uniform(0.25,1.00)
-    rotation = np.array([np.random.uniform(-0.02, 0.02),np.random.uniform(-0.02, 0.02),np.random.uniform(0, np.pi)])
+    #rotation = np.array([np.random.uniform(-0.02, 0.02),np.random.uniform(-0.02, 0.02),np.random.uniform(0, np.pi)])
+    rotation = np.array([np.random.uniform(-0.2, 0.2),np.random.uniform(-0.2, 0.2),np.random.uniform(0, np.pi)])
     bpy.ops.curve.primitive_nurbs_path_add(radius=1.0, enter_editmode=False, align='WORLD', location=location, rotation=rotation, scale=(1,1,1))
     bpy.ops.object.editmode_toggle()
 
     bpy.ops.curve.subdivide(number_cuts=2) 
-    #bpy.ops.curve.subdivide(number_cuts=3) 
     bpy.ops.object.editmode_toggle()
     path = bpy.context.object
 
@@ -234,7 +229,7 @@ def make_noodle():
 
     return path
 
-def make_noodle_pile(n_noodles):
+def make_noodle_pile(n_noodles, settle_time=30):
     noodles = []
     bpy.ops.object.select_all(action='DESELECT')
     for i in range(n_noodles):
@@ -246,11 +241,10 @@ def make_noodle_pile(n_noodles):
     noodles = bpy.context.object
     noodles.pass_index = 1
 
-    for step in range(0, 30, 1):
+    for step in range(bpy.context.scene.frame_current, bpy.context.scene.frame_current + settle_time):
         bpy.context.scene.frame_set(step)
 
-    empties = None
-    return noodles, empties
+    return noodles
 
 def delete_objs(objs):
     bpy.ops.object.select_all(action='DESELECT')
@@ -259,8 +253,9 @@ def delete_objs(objs):
     bpy.ops.object.delete()
 
 
-def push(pusher, start_frame, push_duration, lift_duration, push_start_2d, push_end_2d, hull_2d, densest_3d, annot_dir='annots'):
-    #push_end_2d = densest_3d[:-1]
+def push(pusher, push_duration, lift_duration, push_start_2d, push_end_2d, hull_2d, densest_3d, annot_dir='annots'):
+    start_frame = bpy.context.scene.frame_current
+    
     push_end_2d = [0,0]
 
     offset = push_end_2d - push_start_2d
@@ -286,19 +281,20 @@ def push(pusher, start_frame, push_duration, lift_duration, push_start_2d, push_
         bpy.context.scene.frame_set(step)
         x,y,z = pusher.matrix_world.translation
         pixels = annotate([[x,y,z]] + [[h[0],h[1],0] for h in hull_2d] + [densest_3d])
-        #bpy.context.scene.camera.location = [densest_2d[0], densest_2d[1], bpy.context.scene.camera.location[2]]
         render(step-30)
         np.save('%s/%03d.npy'%(annot_dir,step-30), np.array([pixels]))
 
+    #render(step-30)
+
     return start_frame+push_duration
 
-def wait(start_frame, wait_duration):
+def wait(wait_duration):
+    start_frame = bpy.context.scene.frame_current
     for step in range(start_frame, start_frame+wait_duration):
         bpy.context.scene.frame_set(step)
-        render(step-30)
-    return start_frame+wait_duration
 
-def twirl(fork, start_frame, down_duration, twirl_duration, scoop_duration, twirl_start_3d, angle, annot_dir='annots'):
+def twirl(fork, down_duration, twirl_duration, scoop_duration, wait_duration, twirl_start_3d, angle, annot_dir='annots'):
+    start_frame = bpy.context.scene.frame_current
     x,y,z = twirl_start_3d
 
     fork.location = (x,y,2.0)
@@ -326,6 +322,11 @@ def twirl(fork, start_frame, down_duration, twirl_duration, scoop_duration, twir
         pixels = annotate([twirl_start_3d])
         render(step-30)
         np.save('%s/%03d.npy'%(annot_dir,step-30), np.array([pixels]))
+
+
+    wait(wait_duration)
+
+    #render(step+wait_duration-30)
 
 def densest_point(noodles):
     start = time.time()
@@ -388,24 +389,22 @@ def densest_point_angle(noodles):
     densest_point_neighbor = noodles.matrix_world@densest_point_neighbor.co
     densest_point_neighbor = densest_point_neighbor[:3]
 
-    #neighboring_noodle_points = [densest_point, densest_point_neighbor]
     offset = np.array(densest_point_neighbor[:2]) - np.array(densest_point[:2])
     angle = np.arctan(offset[1]/offset[0])
 
     return densest_point, angle
 
-def reset_pusher(pusher, frame):
-    pusher.location = (0,0,15)
+def reset_pusher(pusher):
+    pusher.location = (15,0,0)
     pusher.rotation_euler = (0,np.pi/2,0)
-    #pusher.rigid_body.kinematic = False
-    #pusher.keyframe_insert(data_path="location", frame=frame)
-    #pusher.keyframe_insert(data_path="rotation_euler", frame=frame)
+    pusher.keyframe_insert(data_path="location", frame=bpy.context.scene.frame_current)
+    pusher.keyframe_insert(data_path="rotation_euler", frame=bpy.context.scene.frame_current)
 
-def reset_fork(fork, frame):
-    fork.location = (0,0,15)
+def reset_fork(fork):
+    fork.location = (15,0,0)
     fork.rotation_euler = (0,0,0)
-    #fork.keyframe_insert(data_path="location", frame=frame)
-    #fork.keyframe_insert(data_path="rotation_euler", frame=frame)
+    fork.keyframe_insert(data_path="location", frame=bpy.context.scene.frame_current)
+    fork.keyframe_insert(data_path="rotation_euler", frame=bpy.context.scene.frame_current)
 
 def noodle_state(noodles):
     points = []
@@ -462,58 +461,77 @@ def remove_picked_up(noodles):
     bpy.ops.object.editmode_toggle()
     return noodles
 
-
-
-def generate_dataset(episodes):
+def initialize_sim():
     if not os.path.exists('masks'):
         os.mkdir('masks')
     if not os.path.exists('annots'):
         os.mkdir('annots')
 
-    render_size = (140,140)
-    #render_size = (64,64)
+    #render_size = (140,140)
+    render_size = (64,64)
     set_render_settings('CYCLES', render_size)
     clear_scene()
     camera = add_camera_light()
     params = {"table_size":10}
     make_table(params)
-    make_pusher()
     make_noodle_rig()
     initialize_renderer()
-
-    for episode in range(episodes):
-        noodles, empties = make_noodle_pile(15)
-        render(episode)
-        #delete_objs([noodles])
-
-    # Grouping
     pusher = make_pusher()
-    areas = []
-    i = 0
-    area = float('inf')
-    while area > 6:
-        if i>5:
-            break
-        freeze_softbody_physics(noodles)
-        hull_2d, center_2d, furthest_2d, area, densest_3d = noodle_state(noodles)
-        add_softbody_physics(noodles)
-        areas.append(area)
-        start = push(pusher, 30+(i*15), 10, 5, furthest_2d, center_2d, hull_2d, densest_3d)
-        i+=1
-    reset_pusher(pusher, bpy.context.scene.frame_current)
+    reset_pusher(pusher)
+    fork = make_fork('assets/fork.stl')
+    reset_fork(fork)
+    return pusher, fork
 
-    start_frame = 30+(i*15)
+def reset_sim(pusher, fork, num_noodles):
+    reset_pusher(pusher)
+    reset_fork(fork)
+    noodles = make_noodle_pile(num_noodles)
+    return noodles
+
+def take_push_action(pusher, noodles):
+    freeze_softbody_physics(noodles)
+    hull_2d, center_2d, furthest_2d, area, densest_3d = noodle_state(noodles)
+    add_softbody_physics(noodles)
+    push_duration = 10
+    lift_duration = 3
+    start = push(pusher, push_duration, lift_duration, furthest_2d, center_2d, hull_2d, densest_3d)
+
+def take_twirl_action(fork, noodles):
     freeze_softbody_physics(noodles)
     densest_3d, angle = densest_point_angle(noodles)
     add_softbody_physics(noodles)
-    fork = make_fork('assets/fork.stl')
-
-    twirl(fork, 30+start_frame, 5, 20, 20, densest_3d, angle)
+    down_duration = 5
+    #twirl_duration = 20
+    twirl_duration = 10
+    #lift_duration = 20
+    lift_duration = 10
+    wait_duration = 10
+    twirl(fork, down_duration, twirl_duration, lift_duration, wait_duration, densest_3d, angle)
     freeze_softbody_physics(noodles)
     remove_picked_up(noodles)
-    reset_fork(fork, bpy.context.scene.frame_current)
+    add_softbody_physics(noodles)
+    reset_fork(fork)
 
-    wait(30+start_frame+45,30)
+
+def generate_dataset(episodes):
+    pusher, fork = initialize_sim()
+    initial_noodles = 15
+    noodles = reset_sim(pusher, fork, initial_noodles)
+
+    initial_area, initial_num_noodles = get_reward(noodles)
+    rewards = []
+    for i in range(10):
+        if random.random()<0.8:
+            take_push_action(pusher, noodles)
+        else:
+            take_twirl_action(fork, noodles)
+        area, num_noodles = get_reward(noodles)
+        rewards.append([initial_area-area, initial_num_noodles-num_noodles])
+        initial_area = area
+        initial_num_noodles = num_noodles
+
+    print('rewards: area, noodles')
+    print(rewards)
 
 if __name__ == '__main__':
     generate_dataset(1)
